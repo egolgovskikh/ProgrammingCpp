046b0dccb2aa8af19efd11aafda854e84c100a94
Евгений Голговских <egolgovskikh@gmail.com>
2018-03-15 13:17:21 +0300

    add 1,16

---
+++ LaboratoryWorks1/LaboratoryWorks1/1,16.hpp
@@ -0,0 +1,14 @@
+//
+//  1,16.hpp
+//  LaboratoryWorks1
+//
+//  Created by jenya on 14.03.2018.
+//  Copyright © 2018 yevtea. All rights reserved.
+//
+
+#ifndef __16_hpp
+#define __16_hpp
+
+#include <stdio.h>
+
+#endif /* __16_hpp */
---
+++ LaboratoryWorks1/LaboratoryWorks1/2, 17.cpp
@@ -0,0 +1,9 @@
+//
+//  2, 17.cpp
+//  LaboratoryWorks1
+//
+//  Created by jenya on 15.03.2018.
+//  Copyright © 2018 yevtea. All rights reserved.
+//
+
+#include "2, 17.hpp"
